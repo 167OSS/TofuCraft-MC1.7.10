b6450fb75d00a747af855757d3e0c8bf74bb28d2
yoruki_167 <78334210+167OSS@users.noreply.github.com>
2025-12-09 13:48:26 +0800

    Update TcChunkProviderEvent.java

--- src/main/java/tsuteto/tofu/world/TcChunkProviderEvent.java
+++ src/main/java/tsuteto/tofu/world/TcChunkProviderEvent.java
@@ -1,35 +1,34 @@
 package tsuteto.tofu.world;
 
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import java.util.Random;
+
 import net.minecraft.world.World;
 import net.minecraft.world.biome.BiomeGenBase;
 import net.minecraft.world.gen.feature.WorldGenMinable;
 import net.minecraft.world.gen.feature.WorldGenerator;
 import net.minecraftforge.event.terraingen.DecorateBiomeEvent;
 import net.minecraftforge.event.terraingen.OreGenEvent;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import tsuteto.tofu.Settings;
 import tsuteto.tofu.init.TcBlocks;
 import tsuteto.tofu.world.gen.feature.WorldGenCrops;
 
-import java.util.Random;
+public class TcChunkProviderEvent {
 
-public class TcChunkProviderEvent
-{
+    private final WorldGenerator tofuOreGen = new WorldGenMinable(TcBlocks.oreTofu, 4);
+
     @SubscribeEvent
-    public void decorateBiome(DecorateBiomeEvent.Post event)
-    {
+    public void decorateBiome(DecorateBiomeEvent.Post event) {
         World worldObj = event.world;
         Random rand = event.rand;
 
-        // Hellsoybeans
-        if (rand.nextInt(600) < Math.min((Math.abs(event.chunkX) + Math.abs(event.chunkZ)) / 2, 400) - 100)
-        {
-            if (worldObj.getBiomeGenForCoords(event.chunkX, event.chunkZ).biomeID == BiomeGenBase.hell.biomeID)
-            {
+        if (rand.nextInt(600) < Math.min((Math.abs(event.chunkX) + Math.abs(event.chunkZ)) / 2, 400) - 100) {
+            if (worldObj.getBiomeGenForCoords(event.chunkX, event.chunkZ).biomeID == BiomeGenBase.hell.biomeID) {
                 int k = event.chunkX;
                 int l = event.chunkZ;
 
-                for (int i = 0; i < 10; ++i)
-                {
+                for (int i = 0; i < 10; ++i) {
                     int j1 = k + rand.nextInt(16) + 8;
                     int k1 = rand.nextInt(128);
                     int l1 = l + rand.nextInt(16) + 8;
@@ -39,18 +38,16 @@ public class TcChunkProviderEvent
         }
     }
 
-    private final WorldGenerator tofuOreGen = new WorldGenMinable(TcBlocks.oreTofu, 4);
-
     @SubscribeEvent
-    public void generateTofuOres(OreGenEvent.Post event)
-    {
+    public void generateTofuOres(OreGenEvent.Post event) {
+        if (Settings.disableTofuOreGeneration) return;
+
         World worldObj = event.world;
         Random rand = event.rand;
         int x = event.worldX;
         int y = event.worldZ;
 
-        for (int l = 0; l < 8; ++l)
-        {
+        for (int l = 0; l < 8; ++l) {
             int i1 = x + rand.nextInt(16);
             int j1 = rand.nextInt(82 - 45) + 45;
             int k1 = y + rand.nextInt(16);
